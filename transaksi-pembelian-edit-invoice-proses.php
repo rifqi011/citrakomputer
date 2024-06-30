c6621be02a96d151fd8e37e1bf8a15df7296893d
rifqi011 <rifqibanusafingi11@gmail.com>
2024-06-30 11:12:22 +0700

    update invoice pembelian

--- transaksi-pembelian-edit-invoice-proses.php
+++ transaksi-pembelian-edit-invoice-proses.php
@@ -1,25 +1,19 @@
-<?php  
-	include 'aksi/functions.php';
-	$id                                    = $_POST['invoice_pembelian_number_id'];
-	$invoice_pembelian_number_input        = $_POST['invoice_pembelian_number_input'];
+<?php
+include 'aksi/functions.php';
+$id                                    = $_POST['invoice_pembelian_number_id'];
+$invoice_pembelian_number_input        = $_POST['invoice_pembelian_number_input'];
 
 
-			if (empty($data['error'])) {
-					$query = "UPDATE invoice_pembelian_number SET
+if (empty($data['error'])) {
+    $query = "UPDATE invoice_pembelian_number SET
 						invoice_pembelian_number_input          = '$invoice_pembelian_number_input'
 						WHERE invoice_pembelian_number_id = '$id'
 					";
 
-				mysqli_query($conn, $query)
-				or die ("Gagal Perintah SQL".mysql_error());
-				$data['hasil'] = 'sukses';
-		} else {
-			$data['hasil'] = 'gagal';
-		}
-		echo json_encode($data);
-			
-		 
-	
-
-	
-?>
+    mysqli_query($conn, $query)
+        or die("Gagal Perintah SQL" . mysql_error());
+    $data['hasil'] = 'sukses';
+} else {
+    $data['hasil'] = 'gagal';
+}
+echo json_encode($data);
